2fdce9fb604470da09bbd0629542ef8d77f29ae7
geokyr <imgeorgekyr@gmail.com>
2021-04-27 02:06:11 +0300

    final ex2

--- ex2/ex2b.m
+++ ex2/ex2b.m
@@ -1,39 +1,41 @@
-pkg load queueing;
-
 clc;
 clear all;
 close all;
 
+pkg load queueing;
+
 % TASK: Plot the charts of utilization, response time, average number of 
 % customers and throughput for the allowed mu's.
 
-l = [5, 5, 5, 5, 5];
-mu = [6, 7, 8, 9, 10];
+counter = 1;
 
-for i=0:4
-  [util, resp, numb, through] = qsmm1(l,mu);
+l = 5;
+mu = 5.1:0.1:10;
+
+for i=1:columns(mu)
+  [util(i), resp(i), numb(i), through(i)] = qsmm1(l,mu(i));
 endfor
 
-figure(1);
+figure(counter++);
 plot(mu, util);
-xlabel ("Service Rate (mu)");
-ylabel ("Utilization of server");
-title ("Utilization of server as a function of service rate");
+xlabel("Service Rate (mu)");
+ylabel("Utilization of server");
+title("Utilization of server as a function of service rate");
 
-figure(2);
+figure(counter++);
 plot(mu, resp);
-xlabel ("Service Rate (mu)");
-ylabel ("Response time of server");
-title ("Response time of server as a function of service rate");
+xlabel("Service Rate (mu)");
+ylabel("Response time of server");
+title("Response time of server as a function of service rate");
 
-figure(3);
+figure(counter++);
 plot(mu, numb);
-xlabel ("Service Rate (mu)");
-ylabel ("Average number of requests on server");
-title ("Average number of requests on server as a function of service rate");
+xlabel("Service Rate (mu)");
+ylabel("Average number of requests on server");
+title("Average number of requests on server as a function of service rate");
 
-figure(4);
+figure(counter++);
 plot(mu, through);
-xlabel ("Service Rate (mu)");
-ylabel ("Throughput of server");
-title ("Throughput of server as a function of service rate");
+xlabel("Service Rate (mu)");
+ylabel("Throughput of server");
+title("Throughput of server as a function of service rate");
--- ex2/ex2c.m
+++ ex2/ex2c.m
@@ -6,6 +6,7 @@ pkg load queueing;
 
 % TASK: Simulate an M/M/1/4 system.
 counter = 1;
+
 l = 5;
 mu = 10;
 
@@ -26,14 +27,15 @@ disp(transition_matrix);
 
 % (ii) Get the ergodic probabilities of the system and plot the bar for bar chart.
 P = ctmc(transition_matrix);
-printf("The ergodic probabilities of the system are: \n");
-disp(P);
+printf("\nThe ergodic probabilities of the system are: \n");
+ergodicP = sprintf("%d  ", P);
+printf("%s \n", ergodicP);
 
 figure(counter++);
-bar(states, P, 1);
-xlabel ("Number of customers");
-ylabel ("Ergodic probability");
-title ("Ergodic probabilities as a function of number of customers");
+bar(states, P, 0.5);
+xlabel("Number of customers");
+ylabel("Ergodic probability");
+title("Ergodic probabilities as a function of number of customers");
 
 % (iii) Get the average number of customers on stationary state.
 average = 0;
@@ -41,11 +43,11 @@ for i=1:4;
   average += i*P(i+1);
 endfor
 
-printf("The average number of customers on stationary state is: %d. \n", average);
+printf("\nThe average number of customers on stationary state is: %d. \n", average);
 
 % (iv) Get the blocking probability.
 blocking_probability = P(end);
-printf("The blocking probability of the system is: %d. \n", blocking_probability);
+printf("\nThe blocking probability of the system is: %d. \n", blocking_probability);
 
 % (v) Transient probability of all the states until they converge to their ergodic probability. 
 % Convergence takes place when Prob and P differ by 0.01.
@@ -65,25 +67,25 @@ endfor
 
 T = 0:0.01:T;
 figure(counter++);
-plot(T, Prob1);
-figure(counter++);
-plot(T, Prob2);
-figure(counter++);
-plot(T, Prob3);
-figure(counter++);
-plot(T, Prob4);
-figure(counter++);
-plot(T, Prob5);
+plot(T, Prob1, T, Prob2, T, Prob3, T, Prob4, T, Prob5);
+xlabel("Time scale in seconds");
+ylabel("Transient probability");
+title(sprintf("Transient probabilities until covergence to ergodic - Service Rate = %d", mu));
+legend("State 1 with 0 customers", "State 2 with 1 customers", "State 3 with 2 customers", "State 4 with 3 customers", "State 5 with 4 customers");
 
 % (vi) Repeat the (v) part with different mu values.
-mu_ = [1, 5, 20];
+mu = [1, 5, 20];
 
-for i=1:3
-  mu = mu_(i);
-  deaths_D = [mu, mu, mu, mu];
+for i=1:columns(mu)
+  deaths_D = [mu(i), mu(i), mu(i), mu(i)];
   transition_matrix = ctmcbd(births_B, deaths_D);
   P = ctmc(transition_matrix);
   
+  Prob1 = [0];
+  Prob2 = [0];
+  Prob3 = [0];
+  Prob4 = [0];
+  Prob5 = [0];
   index = 0;
   for T = 0:0.01:50
     index++;
@@ -100,13 +102,9 @@ for i=1:3
 
   T = 0:0.01:T;
   figure(counter++);
-  plot(T, Prob1);
-  figure(counter++);
-  plot(T, Prob2);
-  figure(counter++);
-  plot(T, Prob3);
-  figure(counter++);
-  plot(T, Prob4);
-  figure(counter++);
-  plot(T, Prob5);
+  plot(T, Prob1, T, Prob2, T, Prob3, T, Prob4, T, Prob5);
+  xlabel("Time scale in seconds");
+  ylabel("Transient probability");
+  title(sprintf("Transient probabilities until covergence to ergodic - Service Rate = %d", mu(i)));
+  legend("State 1 with 0 customers", "State 2 with 1 customers", "State 3 with 2 customers", "State 4 with 3 customers", "State 5 with 4 customers");
 endfor
